fe509f53e8aa01dbdab6abdbf4c17028217e2017
yastaraus <48056887+yastaraus@users.noreply.github.com>
2021-02-19 00:57:15 +0300

    Toyota brand Identity Styling

--- helper/app.js
+++ helper/app.js
@@ -14,27 +14,44 @@ const clearLocalDataBtnElem = document.getElementById('clear-local-data-btn');
 let cars = [];
 
 const addCar = function(ev) {
-    ev.preventDefault(); // Отменяем стандартыную отпраку по клику
+    ev.preventDefault(); // Отменяем стандартное поведение кнопки
 
-    let model = modelElem.value;
-    let price = priceElem.value;
-    let benefit = benefitElem.value;
+    // readLocalData();
+
+    let model = modelElem.value.trim();
+    let price = priceElem.value.trim();
+    let benefit = benefitElem.value.trim();
+
+    let car = new CarData(model, price, benefit);
+
+    // let modelObj = new Object();
+    // modelObj = {
+    //     price: price,
+    //     halfPrice: halfPrice,
+    //     benefit: benefit,
+    //     benefitPrice: benefitPrice,
+    //     lmp: lmp,
+    // }; // Создаем новый объект
+
+    cars.push(car); // Добавляем Объект в массив
+    // writeLocalData(cars); // Пишем в local storage
+    document.querySelector('form').reset(); // Чистим форму после
+    console.log(cars);
+}
+
+function CarData(model, price, benefit) {
     let halfPrice = String(half(price));
     let benefitPrice = String(price - benefit);
     let lmp = String(getLmpPayment(price));
 
-    let car = {
-        model: model,
-        price: price,
-        halfPrice: halfPrice,
-        benefit: benefit,
-        benefitPrice: benefitPrice,
-        lmp: lmp,
-    } // Создаем новый объект
-    cars.push(car); // Добавляем Объект в массив
-    writeLocalData(cars); // Пишем в local storage
-    document.querySelector('form').reset(); // Чистим форму после
+    this.model = model;
+    this.price = price;
+    this.benefit = benefit;
+    this.halfPrice = halfPrice;
+    this.benefitPrice = benefitPrice;
+    this.lmp = lmp;    
 }
+
 
 // Получаем половину числа
 function half(number) {
@@ -49,21 +66,29 @@ function getLmpPayment(number) {
 
 // Пишем в local storage
 function writeLocalData(arr) {
-    localStorage.setItem('cars_data', JSON.stringify(arr));
+    localStorage.setItem('сarsData', JSON.stringify(arr));
 };
 
 // Читаем из local storage
 function readLocalData() {
-    if (localStorage.getItem('cars_data')) {
-        сars = localStorage.getItem('cars_data');
+    if (localStorage.getItem('сarsData')) {
+        сars = JSON.parse(localStorage.getItem('сarsData'));
     } // Проверка на пустой local storage
 };
 
-// Чистим экран и результаты игры
+// function readLocalData() {
+//     if (localStorage.getItem('сarsData') === null) {
+//         localStorage.setItem('сarsData', 0);
+//     }
+//     сars = JSON.parse(localStorage.getItem('сarsData'));
+// };
+
+// Чистим дату
 let dataWipe = function clearLocalData() {
-    cars_data = 0;
-    writeLocalData(cars_data);
+    сarsData = [];
+    writeLocalData(сarsData);
 };
 
 calcBtnElem.addEventListener('click', addCar); // Слушаем кнопку и выполняем addCar
 clearLocalDataBtnElem.addEventListener('click', dataWipe); // Слушаем кнопку и стираем всю дату
+
